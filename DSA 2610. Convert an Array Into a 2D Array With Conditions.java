50c99b67ab92e4c8657bfbfa7fe909dbd56f5095
NItish Kumar <75878650+nitish72@users.noreply.github.com>
2024-01-02 12:57:32 +0530

    Create DSA 2610. Convert an Array Into a 2D Array With Conditions.java

---
+++ DSA 2610. Convert an Array Into a 2D Array With Conditions.java
@@ -0,0 +1,18 @@
+// Dete : 02 Jan 2024
+//https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions/?envType=daily-question&envId=2024-01-02
+class Solution {
+    public List<List<Integer>> findMatrix(int[] nums) {
+        // The number of rows we need equals the maximum frequency.
+    List<List<Integer>> ans = new ArrayList<>();
+    int[] count = new int[nums.length + 1];
+
+    for (final int num : nums) {
+      // Construct `ans` on demand.
+      if (++count[num] > ans.size())
+        ans.add(new ArrayList<>());
+      ans.get(count[num] - 1).add(num);
+    }
+
+    return ans;
+    }
+}
