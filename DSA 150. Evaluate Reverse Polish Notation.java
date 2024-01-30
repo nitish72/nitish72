b141d66923b6e1bddaa0dfde3b491e82c80683c2
NItish Kumar <75878650+nitish72@users.noreply.github.com>
2024-01-30 11:55:22 +0530

    Create DSA 150. Evaluate Reverse Polish Notation.java

---
+++ DSA 150. Evaluate Reverse Polish Notation.java
@@ -0,0 +1,40 @@
+// Date : 50th Jan 2024
+// https://leetcode.com/problems/evaluate-reverse-polish-notation/description/?envType=daily-question&envId=2024-01-30
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+         Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            if (isOperator(token)) {
+                int operand2 = stack.pop();
+                int operand1 = stack.pop();
+                int result = performOperation(operand1, operand2, token);
+                stack.push(result);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+
+    private boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
+    }
+
+    private int performOperation(int operand1, int operand2, String operator) {
+        switch (operator) {
+            case "+":
+                return operand1 + operand2;
+            case "-":
+                return operand1 - operand2;
+            case "*":
+                return operand1 * operand2;
+            case "/":
+                return operand1 / operand2;
+            default:
+                throw new IllegalArgumentException("Invalid operator: " + operator);
+        } 
+    }
+}
