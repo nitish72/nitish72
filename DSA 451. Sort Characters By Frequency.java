d0fec71f866f68cd8aa114ed272716db72f1fb4d
NItish Kumar <75878650+nitish72@users.noreply.github.com>
2024-02-07 21:17:32 +0530

    Create DSA 451. Sort Characters By Frequency.java

---
+++ DSA 451. Sort Characters By Frequency.java
@@ -0,0 +1,32 @@
+// Date : 07 Feb 2024
+// https://leetcode.com/problems/sort-characters-by-frequency/?envType=daily-question&envId=2024-02-07
+class Solution {
+    public String frequencySort(String s) {
+          if (s == null || s.length() == 0) {
+            return s;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        int maxFreq = Integer.MIN_VALUE;
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            maxFreq = Math.max(maxFreq, map.get(ch));
+        }
+        List<List<Character>> buckets = new ArrayList<>();
+        for (int i = 0; i <= maxFreq; i++) {
+            buckets.add(new ArrayList<Character>());
+        }
+        for (Character key : map.keySet()) {
+            int freq = map.get(key);
+            buckets.get(freq).add(key);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int bucketVal = buckets.size() - 1; bucketVal >= 1; bucketVal--) {
+            for (Character ch : buckets.get(bucketVal)) {
+                for (int val = 0; val < bucketVal; val++) {
+                    sb.append(ch);
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
