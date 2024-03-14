ae6d18b0d66334c0011f4946086e226c03c4df0e
NItish Kumar <75878650+nitish72@users.noreply.github.com>
2024-03-14 12:04:09 +0530

    Create DSA 2485. Find the Pivot Integer.java

---
+++ DSA 2485. Find the Pivot Integer.java
@@ -0,0 +1,29 @@
+// Date : 13th March 2024
+// https://leetcode.com/problems/find-the-pivot-integer/
+
+class Solution {
+    public int pivotInteger(int n) {
+        if (n == 1) { 
+            return n;
+        }
+        int leftValue = 1;
+        int rightValue = n;
+        int sumLeft = leftValue;
+        int sumRight = rightValue;
+        while (leftValue < rightValue) {
+            if (sumLeft < sumRight) {
+                leftValue++;
+                sumLeft += leftValue;
+            } 
+            else {
+                rightValue--;
+                sumRight += rightValue;
+            }
+            if (sumLeft == sumRight && leftValue + 1 == rightValue - 1) {
+                return leftValue + 1;
+            }
+        }
+        return -1;
+
+    }
+}
